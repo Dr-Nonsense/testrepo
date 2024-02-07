27e5f3bf6d15f6f69c926871faa492c33abbfdff
SerainaCappelli <seraina.cappelli@gmail.com>
2024-02-07 13:43:48 -0600

    talke some more tomyself in branchybranch

--- trybranching.R
+++ trybranching.R
@@ -1,1 +1,6 @@
 # okok lets talk to myself. This is a new file to be branched and remerged soon. we are in the main branch now
+
+
+# wellwell, I am now in branchybranch no 1 and am adding this conversation to myself
+
+# sooo, but this looks right in both branches. Is this not working or what?
