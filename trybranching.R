df1334a8ad0754abd477d3f430c204b5c78d39bc
SerainaCappelli <seraina.cappelli@gmail.com>
2024-02-07 17:40:38 -0600

    add some text in branch while main has already been changed

--- trybranching.R
+++ trybranching.R
@@ -1,5 +1,11 @@
 original text 1
+text added in server while main is being changed 1
+text added in server while main is being changed 2
 original text 2
+text added in server while main is being changed 3
 original text 3
+text added in server while main is being changed 4
 original text 4
+text added in server while main is being changed 5
 original text 5
+text added in server while main is being changed 6
