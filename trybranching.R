cecced36c65844471dc7ff7dad9b9ff42274b73a
SerainaCappelli <seraina.cappelli@gmail.com>
2024-02-07 13:45:34 -0600

    talk to myself in the main branch after talking to myself in branchybranch

--- trybranching.R
+++ trybranching.R
@@ -1,1 +1,3 @@
 # okok lets talk to myself. This is a new file to be branched and remerged soon. we are in the main branch now
+
+# just to mess with things a bit, lets add something here in the main branch. because this is my actual problem here
